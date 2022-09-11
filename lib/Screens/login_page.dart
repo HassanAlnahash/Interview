b6b30ce8b7fc3c2b2cb0c735996499a849d189ae
Hassan <73310528+MrGhostFox@users.noreply.github.com>
2022-09-11 23:14:02 +0300

    this is the first commit

---
+++ lib/Screens/login_page.dart
@@ -0,0 +1,10 @@
+//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+//FirebaseAuth auth = FirebaseAuth.instance;
+
+//UserCredential userCredential = await FirebaseAuth.instance.signInAnonymously();
+   
+
+
+
